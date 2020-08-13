121c96b1f5237265afa2f7b012b8ff9005c352ab
ahmedmeshref <a.meshref@alustudent.com>
2020-08-13 22:10:04 +0200

    Add static files

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
+let engines = require('consolidate');
 const app = express();
 
 // configure static files
-app.use(express.static('static'))
+app.use(express.static('views'));
+app.use(express.static('public'));
 
 
-const port = 8080;
+
+
+/*
+Routes
+ */
+app.get('/', (req, res) => {
+    res.render('index.html');
+})
+
+app.get('/shopping_list', (req, res) => {
+    res.render('list.html');
+})
+
+
+
+const port = 3000;
 const server = app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
 })
