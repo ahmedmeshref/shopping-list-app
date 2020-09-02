8ef0c1f72c594c6cbf2b9ac11696389f9f2b5029
ahmedmeshref <a.meshref@alustudent.com>
2020-09-02 03:45:55 +0200

    Implement delete_item

--- public/js/delete_item.js
+++ public/js/delete_item.js
@@ -2,10 +2,26 @@ const keys = {
     items_wrapper: document.getElementById("table-body")
 }
 
+let sendDeleteRequest = async (data) => {
+    let response = await fetch("/list", {
+        method: "DELETE",
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    return response.json();
+}
+
 let delete_item = (e) => {
     const target = e.target
-    console.log(target.classList.contains("fa-check"))
-    console.log(target)
+    // verify the click target to be the check btn
+    if (!target.classList.contains("fa-check")) return;
+    sendDeleteRequest({
+        id: target.dataset.id
+    })
+        .then()
 }
 
 keys.items_wrapper.addEventListener("click", delete_item)
